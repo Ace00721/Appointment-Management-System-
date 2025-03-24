ef2c6b513cf29e3b46ad1e9b07f2c8de69159aa8
Ace00721 <jeremyjuarez03@yahoo.com>
2025-03-24 09:43:21 -0700

    Add files via upload

---
+++ Appoitment info.py
@@ -0,0 +1,17 @@
+class AppointmentInfo:
+    def __init__(self, app_date: str, app_time: str, app_employee: str, app_request: str, app_service: str):
+        self.app_date = app_date
+        self.app_time = app_time
+        self.app_employee = app_employee
+        self.app_request = app_request
+        self.app_service = app_service
+
+    def set_app(self, app_date: str, app_time: str):
+        self.app_date = app_date
+        self.app_time = app_time
+
+    def display_date(self):
+        return f"Appointment Date: {self.app_date}, Time: {self.app_time}"
+
+    def show_summ(self):
+        return f"Appointment Summary: \nDate: {self.app_date} \nTime: {self.app_time} \nEmployee: {self.app_employee} \n Request: {self.app_request} \nService: {self.app_service}"
