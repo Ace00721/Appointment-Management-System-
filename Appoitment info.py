92d59bd69f39f2492daa36fffd91c22d35baf1f5
Ace00721 <jeremyjuarez03@yahoo.com>
2025-04-09 11:15:39 -0700

    Appointment info.py

--- Appoitment info.py
+++ Appoitment info.py
@@ -1,10 +1,10 @@
 class AppointmentInfo:
-    def __init__(self, app_date: str, app_time: str, app_employee: str, app_request: str, app_service: str):
+    def __init__(self, app_date: str, app_time: str, app_employee: str, app_servicet: str, app_payment: str):
         self.app_date = app_date
         self.app_time = app_time
         self.app_client = app_client
-        self.app_request = app_request
-        self.app_service = app_service
+        self.app_request = app_service
+        self.app_service = app_payment
 
     def set_app(self, app_date: str, app_time: str):
         self.app_date = app_date
@@ -13,11 +13,11 @@ class AppointmentInfo:
     def display_date(self):
         return f"Appointment Date: {self.app_date}, Time: {self.app_time}"
 
-    def show_summ(self):
-        return f"Appointment Summary: \nDate: {self.app_date} \nTime: {self.app_time} \nEmployee: {self.app_client} \n Request: {self.app_request} \nService: {self.app_service}"
+    def get_appointment(self):
+        return f"Appointment Summary: \nDate: {self.app_date} \nTime: {self.app_time} \nEmployee: {self.app_client} \nService: {self.app_service} \nPayment: {self.app_payment}"
 
 # Example Usage
 if __name__ == "__main__":
-    appointment = AppointmentInfo("2025-05-20", "10:00 AM", "Kyle", "Tattoo", "Standard")
+    appointment = AppointmentInfo("2025-05-20", "10:00 AM", "Kyle", "Tattoo, "$250")
     print(appointment.display_date())
-    print(appointment.show_summ())
+    print(appointment.get_appointment())
